f3bdddd9194f0457e2b5180fdab980402f20e039
Developer <Developer@dev.com>
2023-01-31 11:52:46 +0530

    commit..

--- IdeaJFSProject/LD2-Hibernate-MySql/src/main/java/com/demo/library/Library.java
+++ IdeaJFSProject/LD2-Hibernate-MySql/src/main/java/com/demo/library/Library.java
@@ -4,23 +4,48 @@ package com.demo.library;
 
 //import org.hibernate.annotations.Entity;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
-//@Table(name="LIBRARIES")
+@Table(name="LIBRARIES")
 //@Column
 public class Library {
 
     @Id
+    @Column(name="ID")
     private int id;
+    @Column(name="NAME")
     private String name;
 
+    //PRIMARY KEY
+    @OneToMany(mappedBy = "library")
+    private List<Author>  authorArrayList = new ArrayList<>();
     public Library() {
 
     }
     public Library(int id, String name) {
         this.id = id;
         this.name = name;
+    }
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void addAuthor(Author author){
+        authorArrayList.add(author);
     }
 }
