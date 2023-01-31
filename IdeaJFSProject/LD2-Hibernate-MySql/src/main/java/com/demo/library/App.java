9c0fbdf9671c30e573bcaf3c25272d330c287ea2
Developer <Developer@dev.com>
2023-01-31 10:20:55 +0530

    commit..

--- IdeaJFSProject/LD2-Hibernate-MySql/src/main/java/com/demo/library/App.java
+++ IdeaJFSProject/LD2-Hibernate-MySql/src/main/java/com/demo/library/App.java
@@ -1,5 +1,8 @@
 package com.demo.library;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
@@ -14,14 +17,21 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "HIBERNATE" );
+        SessionFactory sessionFactory;
         EntityManagerFactory  entityManagerFactory = Persistence.createEntityManagerFactory("demo.PersistenceUnit1");
         EntityManager entityManager = entityManagerFactory.createEntityManager();
+        Session session = null;
         EntityTransaction entityTransaction = entityManager.getTransaction();
         entityTransaction.begin();
-        Library library1 = new Library(1,"Library1");
-//        Library library1 = new Library(2,"Library2");
+//        Transaction transaction =  session.getTransaction();
+//        session.beginTransaction();
+//        Library library1 = new Library(1,"Library1");
+        Library library1 = new Library(2,"Library2");
         entityManager.persist(library1);
+//        session.save(library1);
         entityTransaction.commit();
+//       transaction.commit();
+//       session.close();
         entityManagerFactory.close();
     }
 }
