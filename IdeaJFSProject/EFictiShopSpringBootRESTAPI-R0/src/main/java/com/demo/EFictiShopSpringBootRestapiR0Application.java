0c73a68f44f08f5f8a1da0218f6ef6ed741a8c49
Developer <Developer@dev.com>
2023-02-02 10:52:59 +0530

    commit..

--- IdeaJFSProject/EFictiShopSpringBootRESTAPI-R0/src/main/java/com/demo/EFictiShopSpringBootRestapiR0Application.java
+++ IdeaJFSProject/EFictiShopSpringBootRESTAPI-R0/src/main/java/com/demo/EFictiShopSpringBootRestapiR0Application.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//@ComponentScan
+//@EnableAutoConfiguration
+//@Configuration
 public class EFictiShopSpringBootRestapiR0Application { public static void main(String[] args) {
     System.out.println("Standalone core java App!");
         SpringApplication.run(EFictiShopSpringBootRestapiR0Application.class, args);
